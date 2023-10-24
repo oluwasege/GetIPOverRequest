77fb874c78d38c3fec73f64c00cc2810a97a3687
Oluwasegun Akinpelu <akinpelu53@gmail.com>
2023-10-24 21:34:27 +0100

    ....

--- GetIPOverRequest/Program.cs
+++ GetIPOverRequest/Program.cs
@@ -10,15 +10,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseAuthorization();
 
 app.MapControllers();
 
