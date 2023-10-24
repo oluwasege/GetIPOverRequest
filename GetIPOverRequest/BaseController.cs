f7a21795f74831197920e6e01011b1873f1e59b4
Oluwasegun Akinpelu <akinpelu53@gmail.com>
2023-10-24 14:09:54 +0100

    Add project files.

---
+++ GetIPOverRequest/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetIPOverRequest
+{
+    public class BaseController : ControllerBase
+    {
+        protected string? GetIpAddress
+        {
+            get
+            {
+                return Request.Headers.ContainsKey("X-Forwarded-For")
+            ? Request.Headers["X-Forwarded-For"]
+            : HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString() ?? "N/A";
+            }
+        }
+    }
+}
---
+++ GetIPOverRequest/Controllers/GetIPController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetIPOverRequest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class GetIPController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(GetIpAddress);
+        }
+
+    }
+}
